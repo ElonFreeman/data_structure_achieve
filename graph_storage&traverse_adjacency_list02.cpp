f55c175c5b1bc7a288dd5477e9f9430f96358d01
ElonFreeman <zhanghao112358@outlook.com>
2026-06-20 19:21:25 +0800

    26062001

--- graph_storage&traverse_adjacency_list02.cpp
+++ graph_storage&traverse_adjacency_list02.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#include <exception>
 using namespace std;
 
 struct edge
@@ -17,12 +18,39 @@ vector<bool> access_lable;
 
 void BFS(vector<vertex>& vertexs,int start)
 {
-    
+    deque<int> traverse_queue;
+    traverse_queue.push_back(start); access_lable[start]=true;
+
+    while(!(traverse_queue.empty()))
+    {
+        edge *current=vertexs[traverse_queue.front()].firstedge;
+        while(current!=nullptr)
+        {
+            if(access_lable[current->index]==false)
+            {
+                traverse_queue.push_back(current->index); access_lable[current->index]=true;
+            }
+            current=current->next;
+        }
+
+        cout << vertexs[traverse_queue.front()].data << ' '; traverse_queue.pop_front();
+    }
 }
 
 void DFS(vector<vertex>& vertexs,int start)
 {
-    
+    cout << vertexs[start].data << ' '; access_lable[start]=true;
+
+    edge *current=vertexs[start].firstedge;
+    while(current!=nullptr)
+    {
+        if(access_lable[current->index]==false)
+        {
+            DFS(vertexs,current->index);
+        }
+
+        current=current->next;
+    }
 }
 
 int main(void)
