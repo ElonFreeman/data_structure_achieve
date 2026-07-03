a18e546107911372311a16de81220db3a632f19b
ElonFreeman <zhanghao112358@outlook.com>
2026-07-03 20:05:06 +0800

    26070301

--- heap_sort.cpp
+++ heap_sort.cpp
@@ -6,20 +6,47 @@ vector<int> heap;
 
 void sift_up(int index)
 {
-    int leftch=index*2,rightch=index*2+1,largest=index;
-    
+
 }
+void sift_down(int index)
+{
+    int parent=index; 
+    while(parent*2+1<num_element)
+    {
+        int leftch=parent*2+1,rightch=parent*2+2,maxch=leftch;
+        if(rightch<num_element && heap[rightch]>heap[leftch])
+        {
+            maxch=rightch;
+        }
+        if(heap[parent]<heap[maxch])
+        {
+            swap(heap[parent],heap[maxch]); parent=maxch;
+        }
+        else
+        {break;}        
+    }
+}
+
 void generate_heap() //max-heap
 {
     for(int index=num_element/2-1;index>=0;index--)
     {
-        sift_up(index);
+        sift_down(index);
     }
 }
 
 void heap_sort()
 {
+    vector<int> seq_serial;
+    while(!heap.empty())
+    {
+        swap(heap.front(),heap.back());
+        seq_serial.push_back(heap.back()); heap.pop_back(); num_element--;
+        sift_down(0);
+    }
 
+    for(int output:seq_serial)
+    {cout << output << ' ';}
 }
 
 int main(void)
@@ -27,7 +54,8 @@ int main(void)
     cin >>num_element; heap.resize(num_element);
     for(int &input:heap) {cin >> input;}
 
-
+    generate_heap();
+    heap_sort(); cout << endl;
 
 
     return 0;
